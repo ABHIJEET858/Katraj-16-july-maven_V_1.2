b3ef336eec5df3ff0c19408904f0acb34ebf1ff7
ABHIJEET858 <abhijeetkumbhar8586@gmail.com>
2022-12-07 16:22:49 +0530

    Test5

--- Katraj-16-july-maven/src/main/java/pages/HomePage.java
+++ Katraj-16-july-maven/src/main/java/pages/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
-	
+	//this is my first homepage
 	@FindBy(xpath = "//li[@class='oxd-userdropdown']")
 	public WebElement profile;
 	
